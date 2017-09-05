6d0552e282dee2478fb21c78ebe24287b4cdfa95
pabruce <pbruce@mail.bradley.edu>
2017-09-05 11:12:01 -0500

    New Camera System

---
+++ Assets/2-Player Stuff/Scripts/Mangement/DynamicCamera.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicCamera : MonoBehaviour 
+{
+	public float orthographicSize =5;
+	public Transform player1, player2;
+	public float minSizeY = 5f;
+
+
+	void SetCameraPos() 
+	{
+		Vector3 middle = (player1.position + player2.position) * 0.5f;
+
+		GetComponent<Camera>().transform.position = new Vector3
+			(
+			middle.x,
+			middle.y,
+			GetComponent<Camera>().transform.position.z
+		);
+	}
+
+	void SetCameraSize() 
+	{
+		
+		//horizontal size is based on actual screen ratio
+		float minSizeX = minSizeY * Screen.width / Screen.height;
+
+		//multiplying by 0.5, because the ortographicSize is actually half the height
+		float width = Mathf.Abs(player1.position.x - player2.position.x) * 0.5f;
+		float height = Mathf.Abs(player1.position.y - player2.position.y) * 0.5f;
+
+		//computing the size
+		float camSizeX = Mathf.Max(width, minSizeX);
+		//camera.orthographicSize = Mathf.Max(height,
+		//	camSizeX * Screen.height / Screen.width, minSizeY);
+	}
+
+	void Update() 
+	{
+		SetCameraPos();
+		SetCameraSize();
+	}
+
+}
