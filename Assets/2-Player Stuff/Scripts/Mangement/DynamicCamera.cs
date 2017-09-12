9c4dcb8f65eda8caa699767cdc2e4c539a146110
pabruce <pbruce@mail.bradley.edu>
2017-09-12 12:35:33 -0500

    camera

--- Assets/2-Player Stuff/Scripts/Mangement/DynamicCamera.cs
+++ Assets/2-Player Stuff/Scripts/Mangement/DynamicCamera.cs
@@ -7,6 +7,7 @@ public class DynamicCamera : MonoBehaviour
 	public float orthographicSize =120;
 	public Transform player1, player2;
 	public float minSizeY = 15f;
+	public float minSizeX = 15f;
 	public Vector2 minimumBoundary;
 	public Vector2 maximumBoundary;
 
@@ -31,12 +32,11 @@ public class DynamicCamera : MonoBehaviour
 		//multiplying by 0.5, because the ortographicSize is actually half the height
 		float width = Mathf.Abs(player1.position.x - player2.position.x) * 0.5f;
 		float height = Mathf.Abs(player1.position.y - player2.position.y) *0.5f;
-
+		 
 		//computing the size
 		float camSizeX = Mathf.Max(width, minSizeX);
 		Camera.main.orthographicSize = Mathf.Max(height, camSizeX * Screen.height / Screen.width, minSizeY);
 	}
-		
 
 	void Update() 
 	{
