8d529b08e97f0281845bf7d1402c0e716ce09779
pabruce <pbruce@mail.bradley.edu>
2017-09-14 09:47:21 -0500

    Revert "Removed Sarah's Stuff"
    This reverts commit 2cf28e7b16b8f04a6908ab7496a9f6edb4d9bdf6.

---
+++ Assets/GarbagePersonsStuff/ExitScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D colliderHit)
+	{
+		if (colliderHit.CompareTag ("Player"))
+		{
+			SceneManager.LoadScene ("winScreen");
+			//Application.LoadLevel("winScreen");
+		}
+	}
+}
---
+++ Assets/GarbagePersonsStuff/ItemDetector.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDetector : MonoBehaviour {
+
+	public bool isActive = false;
+	public string itemName = "key";
+	public GameObject item;
+	public bool destroysObject = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D colliderHit)
+	{
+		if (colliderHit.CompareTag ("Item") && !isActive && colliderHit.name == itemName)
+		{
+			isActive = true;
+			item = colliderHit.gameObject;
+			if (destroysObject)
+				Destroy (item);
+		}
+		else if (colliderHit.CompareTag ("Player") && !isActive && colliderHit.transform.Find("ItemSlot").Find(itemName) != null)
+		{
+			isActive = true;
+			item = colliderHit.transform.Find("ItemSlot").Find(itemName).gameObject;
+			if (destroysObject)
+				Destroy (item);
+			else {
+				item.transform.SetParent (null);
+				item.GetComponent<CircleCollider2D> ().enabled = true;
+			}
+			colliderHit.GetComponent<Itemgrabber> ().hasItem = false;
+		}
+	}
+}
