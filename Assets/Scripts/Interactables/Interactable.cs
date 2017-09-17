2cf34406021026c5cdbf8f4762417560faa418c9
Kevin Adams <kadams8670@gmail.com>
2017-09-17 13:01:11 -0500

    Interactable Class Revision
    Revised Interactable abstract class to comment out its functionality and provide the base OnInteract() for interactable subclasses

---
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+	//public bool interactable;
+	//public virtual bool activated{ get; protected set; }
+
+	public virtual void OnInteract()
+	{
+		/*
+		if (!interactable)
+			return;
+
+		activated = !activated;
+		*/ 
+	}
+}
