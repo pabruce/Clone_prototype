389a6711d2448dae812e2e5b01e4a97d4029a35b
Kevin Adams <kadams8670@gmail.com>
2017-09-17 13:05:10 -0500

    Conveyor Belt script interaction
    Modified conveyor belt script; added interaction scripts for enabling and reversing conveyor belts

---
+++ Assets/Scripts/Interactables/EnableConveyor_Interactable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ConveyorBelt))]
+public class EnableConveyor_Interactable : Interactable 
+{
+	ConveyorBelt belt; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		belt = GetComponent<ConveyorBelt>(); 
+	}
+	
+	public override void OnInteract()
+	{
+		belt.activated = !belt.activated; 
+	}
+}
--- Assets/Scripts/Manufacturing/ConveyorBelt.cs
+++ Assets/Scripts/Manufacturing/ConveyorBelt.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class ConveyorBelt : MonoBehaviour 
 {
+	public bool activated; 
 	public float speed = 1.0f;
 
 	void OnTriggerStay2D(Collider2D col)
 	{
-		Rigidbody2D rig;
+		if (activated)
+		{
+			Rigidbody2D rig;
 
-		rig = col.gameObject.GetComponent<Rigidbody2D> ();
+			rig = col.gameObject.GetComponent<Rigidbody2D>();
 
-		rig.AddForce (Vector3.right * speed * Time.deltaTime);
-		print ("SHIT WORKS");
+			rig.AddForce(Vector3.right * speed * Time.deltaTime);
+			print("SHIT WORKS");
+		}
 	}
 
 
