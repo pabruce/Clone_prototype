c1edda61870c7a8aaf246bf4307fa837ee7ba79e
The-Alhooness <sarahkmcglasson@gmail.com>
2017-09-19 09:06:38 -0500

    lightning cannon test
    base lightning cannon shit

---
+++ Assets/Scripts/Electric/LightningReciever.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningReciever : MonoBehaviour 
+{
+	public bool isActive;
+	private float timer;
+	private float timeBetweenHits = 0.25f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (timer > 0)
+			timer -= Time.deltaTime;
+		else
+			timer = 0;
+		isActive = timer > 0;
+	}
+		
+	public void Activate()
+	{
+		timer = timeBetweenHits;
+	}
+}
