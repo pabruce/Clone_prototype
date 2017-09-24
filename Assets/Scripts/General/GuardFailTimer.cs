2b34bf6fef2076698242053f0d3b36258fb601a3
pabruce <pbruce@mail.bradley.edu>
2017-09-24 13:54:00 -0500

    PlayerHitBubble Added + Added To Prefabs

---
+++ Assets/Scripts/General/GuardFailTimer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GuardFailTimer : MonoBehaviour 
+{
+	public void FailTimerActivated (float timeToChange)
+	{
+		StartCoroutine (ActivateSceneRoutine (timeToChange));
+	}
+		
+	private IEnumerator ActivateSceneRoutine(float timeToChange)
+	{
+		yield return new WaitForSeconds(timeToChange);
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+}
---
+++ Assets/Scripts/General/GuardTimer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardTimer : MonoBehaviour 
+{
+
+	public void ActivateAfterCollide (float time)
+	{
+		StartCoroutine (ActivateRoutine (time));
+	}
+
+	private IEnumerator ActivateRoutine(float time)
+	{
+		yield return new WaitForSeconds(time);
+		GameObject.FindGameObjectWithTag ("Guard").GetComponent<GuardAI> ().enabled = true;
+	}
+}
